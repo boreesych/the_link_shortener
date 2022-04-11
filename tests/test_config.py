0004ed2fa7ca09e5c4d1faf7881790f05ca84f25
boreesych <bartenev@outlook.com>
2022-04-11 12:47:59 +0100

    fixed env tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,13 +3,17 @@ import os
 
 def test_env_vars():
     assert 'sqlite:///db.sqlite3' in list(os.environ.values()), (
-        'Добавьте переменную окружение для базы данных со значением sqlite:///db.sqlite3'
+        'Проверьте наличие переменной окружения с настройками для подключения'
+        ' базы данных со значением sqlite:///db.sqlite3'
     )
 
 
 def test_config(default_app):
     assert default_app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///db.sqlite3', (
-        'Укажите значение переменной окружения DB'
+        'Проверьте, что конфигурационному ключу SQLALCHEMY_DATABASE_URI '
+        'присвоено значение с настройками для подключения базы данных'
     )
     assert not default_app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
-    assert default_app.config['SECRET_KEY'] == os.getenv('SECRET_KEY')
+    assert default_app.config['SECRET_KEY'] == os.getenv('SECRET_KEY'), (
+        'Проверьте, что конфигурационному ключу SECRET_KEY '
+        'присвоено значение')
