bdd8a6b6d215b73378bd7c96ea93d74e79248a64
Anatoly Korobov <a.korobov06@gmail.com>
2022-04-11 13:28:17 +0300

    check db env var in values not keys

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,15 @@
 import os
 
-from yacut import app
-
 
 def test_env_vars():
-    assert 'DB' in os.environ, 'Добавьте переменную окружение DB'
+    assert 'sqlite:///db.sqlite3' in list(os.environ.values()), (
+        'Добавьте переменную окружение для базы данных со значением sqlite:///db.sqlite3'
+    )
 
 
-def test_config():
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///db.sqlite3', (
+def test_config(default_app):
+    assert default_app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///db.sqlite3', (
         'Укажите значение переменной окружения DB'
     )
-    assert not app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
-    assert app.config['SECRET_KEY'] == os.getenv('SECRET_KEY')
+    assert not default_app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
+    assert default_app.config['SECRET_KEY'] == os.getenv('SECRET_KEY')
