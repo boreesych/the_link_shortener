709cda8bf5cc4136fc58c77aa80a7f405ca8c8af
Anatoly Korobov <a.korobov06@gmail.com>
2022-04-11 13:13:29 +0300

    check db env exists

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,7 +3,13 @@ import os
 from yacut import app
 
 
+def test_env_vars():
+    assert 'DB' in os.environ, 'Добавьте переменную окружение DB'
+
+
 def test_config():
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///db.sqlite3'
+    assert app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///db.sqlite3', (
+        'Укажите значение переменной окружения DB'
+    )
     assert not app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
     assert app.config['SECRET_KEY'] == os.getenv('SECRET_KEY')
