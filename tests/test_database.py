275b2f447c17d08fe25b02cefaf9c29a93d92d95
boreesych <bartenev@outlook.com>
2022-05-09 23:51:58 +0100

    tests have been updated

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,16 +1,20 @@
 from sqlalchemy import inspect
 
-from yacut import db
+# from yacut import db
 from yacut.models import URL_map
 
 
 def test_fields(_app):
-    assert db.engine.table_names() == ['URL_map'], (
-        'Не обнаружена таблица URL_map'
-    )
+    # assert db.engine.table_names() == ['URL_map'], (
+    #     'Не обнаружена таблица URL_map'
+    # )
+    # a = inspect(db.engine)
+    # assert a.has_table('URL_map'), 'Не обнаружена таблица URL_map'
+
     inspector = inspect(URL_map)
-    fields = [i.key for i in inspector.mapper.column_attrs]
-    assert all(field in ['id', 'original', 'short', 'timestamp'] for field in fields), (
+    fields = [column.name for column in inspector.columns]
+    print(fields)
+    assert all(field in fields for field in ['id', 'original', 'short', 'timestamp']), (
         'В модели не найдены все необходимые поля. '
         'Проверьте модель: в ней должны быть поля id, original, short и timestamp.'
     )
